4fff0fe6c17be11087d034cb0ddf8a812d5d299a
Josh Espinoza <joshespi@gmail.com>
2024-10-10 14:56:43 -0600

    cleaned up <br> tags since they conflicted with grid layout

--- src/resources/views/create_asset.blade.php
+++ src/resources/views/create_asset.blade.php
@@ -9,36 +9,36 @@
     <h3>General Information</h3>
     <div id="information">
         <label for="serial">Serial:</label>
-        <input type="text" id="serial" name="serial"><br>
+        <input type="text" id="serial" name="serial">
 
         <label for="barcode">Barcode:</label>
-        <input type="text" id="barcode" name="barcode"><br>
+        <input type="text" id="barcode" name="barcode">
 
         <label for="company">Company:</label>
-        <input type="text" id="company" name="company"><br>
+        <input type="text" id="company" name="company">
 
         <label for="model">Model:</label>
-        <input type="text" id="model" name="model"><br>
+        <input type="text" id="model" name="model">
     </div>
 
     <h3>Location</h3>
     <div id="location">
         <label for="site_number">Site:</label>
         <select id="site_number" name="site_number">
-        @foreach ($sites as $site)
+            @foreach ($sites as $site)
             <option value="{{ $site->site_number }}">{{ $site->display_name }}</option>
-        @endforeach
-        </select><br>
+            @endforeach
+        </select>
 
         <label for="room">Room:</label>
-        <input type="text" id="room" name="room"><br>
+        <input type="text" id="room" name="room">
     </div>
 
 
     <h3>Extra</h3>
     <div id="extra">
         <label for="notes">Additional notes:</label>
-        <input type="text" id="notes" name="notes"><br>
+        <input type="text" id="notes" name="notes">
     </div>
 
     <input type="submit" value="Create Asset">
