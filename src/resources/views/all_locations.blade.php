8fb1fd84d2110c62219bc720dbc183aeebc6be7b
Josh Espinoza <joshespi@gmail.com>
2024-10-10 14:59:41 -0600

    switched to unordered list.

--- src/resources/views/all_locations.blade.php
+++ src/resources/views/all_locations.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
 <h1>All Locations</h1>
-@foreach ($location_list as $loc)
-<div>
-    <a href="{{ route('location_assets', ['location' => $loc['site_number']]) }} ">{{ $loc['display_name'] }}</a>
-</div>
-@endforeach
+<ul>
+    @foreach ($location_list as $loc)
+    <li><a href="{{ route('location_assets', ['location' => $loc['site_number']]) }} ">{{ $loc['display_name'] }}</a></li>
+    @endforeach
+</ul>
 @endsection
