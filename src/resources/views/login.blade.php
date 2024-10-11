af975c8683f83138c762b2ccd2322b74d4447bec
Josh Espinoza <joshespi@gmail.com>
2024-10-11 06:57:20 -0600

    add header to login page.

--- src/resources/views/login.blade.php
+++ src/resources/views/login.blade.php
@@ -1,20 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <title>Maintenance Vault</title>
-        <link rel="stylesheet" href="{{url('assets/css/app.css')}}">
-    </head>
-    <body> 
+
+<head>
+    <title>Maintenance Vault</title>
+    <link rel="stylesheet" href="{{url('assets/css/app.css')}}">
+</head>
+@include('header')
+
+<body>
     <h1>Login</h1>
     @if ($errors->has('login'))
     <div id="login-error-container">
         <strong>
-        {{ $errors->first('login') }}
+            {{ $errors->first('login') }}
         </strong>
     </div>
     @endif
 
     <a href="{{ route('google.redirect') }}" class="btn btn-primary">Login with Google</a>
     @include('footer')
-    </body>
+</body>
+
 </html>
