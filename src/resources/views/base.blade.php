441a2740030d9842460385248ab551496693f4ff
Josh Espinoza <joshespi@gmail.com>
2024-10-11 07:35:22 -0600

    linked in js file

--- src/resources/views/base.blade.php
+++ src/resources/views/base.blade.php
@@ -1,14 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <title>Maintenance Vault</title>
-        <link rel="stylesheet" href="{{url('assets/css/app.css')}}">
-    </head>
-    <body>
+
+<head>
+    <title>Maintenance Vault</title>
+    <link rel="stylesheet" href="{{ asset('assets/css/app.css') }}">
+</head>
+
+<body>
     @include('header')
 
     @yield('content')
 
     @include('footer')
-    </body>
+    <script src="{{ asset('assets/js/main.js') }}"></script>
+</body>
+
 </html>
