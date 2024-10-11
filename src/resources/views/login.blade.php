043c1c44763c18b956da5e9ed86204c3f1478892
Braxton Anderson <braxtona@provo.edu>
2024-10-11 09:48:33 -0600

    add nicer login button

--- src/resources/views/login.blade.php
+++ src/resources/views/login.blade.php
@@ -4,11 +4,11 @@
 <head>
     <title>Maintenance Vault</title>
     <link rel="stylesheet" href="{{url('assets/css/app.css')}}">
+    <link rel="stylesheet" href="{{url('assets/css/login.css')}}">
 </head>
 @include('header')
 
 <body>
-    <h1>Login</h1>
     @if ($errors->has('login'))
     <div id="login-error-container">
         <strong>
@@ -17,7 +17,11 @@
     </div>
     @endif
 
-    <a href="{{ route('google.redirect') }}" class="btn btn-primary">Login with Google</a>
+    <div id="login-wrapper">
+        <h1>Login to Mvault</h1>
+        <a href="{{ route('google.redirect') }}" class="button">Login with Google</a>
+        <p>This system is intended for authorized users only. Any attempts will be logged.</p>
+    </div>
     @include('footer')
 </body>
 
