5022de736e00087637e2d1529c17dbcafd19bb2f
Josh Espinoza <joshespi@gmail.com>
2024-10-14 11:22:18 -0600

    added back

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+
 use Illuminate\Support\Facades;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use App\View\Composers\FooterComposer;
 
