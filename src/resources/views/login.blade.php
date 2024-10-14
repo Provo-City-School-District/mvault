833cc9d63a19f6b4736b314c8307fe91cc8a5013
Josh Espinoza <joshespi@gmail.com>
2024-10-14 13:55:19 -0600

    added assets to login page.

--- src/resources/views/login.blade.php
+++ src/resources/views/login.blade.php
@@ -3,7 +3,10 @@
 
 <head>
     <title>Maintenance Vault</title>
-    <link rel="stylesheet" href="{{asset('assets/css/app.css')}}">
+    <link rel="stylesheet" href="{{ asset('assets/css/reset.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/variables-common.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/typography.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/app.css') }}">
     <link rel="stylesheet" href="{{asset('assets/css/login.css')}}">
 </head>
 @include('header')
