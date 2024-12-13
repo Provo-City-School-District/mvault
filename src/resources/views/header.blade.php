e3c44fa505ed6804c10c32bb4d4d9638d64ceb79
Braxton Anderson <braxtona@provo.edu>
2024-12-13 11:05:35 -0700

    removme spaces in href

--- src/resources/views/header.blade.php
+++ src/resources/views/header.blade.php
@@ -2,12 +2,12 @@
     <img src="{{ asset('assets/img/pcsd-logo-website-header-160w.png') }}" alt="siteLogo" id="siteLogo">
     @auth
     <nav class="topnav" id="myTopnav">
-        <a href=" {{ route('create_asset') }} " class="{{ request()->is('create_asset') ? 'active' : '' }}">Create Asset</a>
-        <a href=" {{ route('all_locations') }} " class="{{ request()->is('all_locations') ? 'active' : '' }}">Locations</a>
-        <a href=" {{ route('search') }} " class="{{ request()->is('search') ? 'active' : '' }}">Search</a>
-        <a href=" {{ route('profile') }} " class="{{ request()->is('profile') ? 'active' : '' }}">Profile</a>
-        <a href=" {{ route('logout') }} ">Logout</a>
-        <a href=" javascript:void(0);" class="icon" onclick="toggleNav()">
+        <a href="{{ route('create_asset') }}" class="{{ request()->is('create_asset') ? 'active' : '' }}">Create Asset</a>
+        <a href="{{ route('all_locations') }}" class="{{ request()->is('all_locations') ? 'active' : '' }}">Locations</a>
+        <a href="{{ route('search') }}" class="{{ request()->is('search') ? 'active' : '' }}">Search</a>
+        <a href="{{ route('profile') }}" class="{{ request()->is('profile') ? 'active' : '' }}">Profile</a>
+        <a href="{{ route('logout') }}">Logout</a>
+        <a href="javascript:void(0);" class="icon" onclick="toggleNav()">
             &#9776;
         </a>
     </nav>
