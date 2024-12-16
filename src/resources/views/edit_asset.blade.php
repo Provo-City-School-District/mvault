afbfd80c569d4023e23f1f59c81b357eb0df670c
Braxton Anderson <braxtona@provo.edu>
2024-12-16 09:49:39 -0700

    fix asset values being cut off

--- src/resources/views/edit_asset.blade.php
+++ src/resources/views/edit_asset.blade.php
@@ -14,19 +14,19 @@
         <h2 class="text-2xl">General Information</h2>
         <div class="grid grid-cols-4 gap-4 mb-10">
             <label for="serial">Serial:</label>
-            <input class="bg-gray-200 text-gray-700 border border-black rounded" type="text" id="serial" name="serial" value={{ $asset->serial }}>
+            <input class="bg-gray-200 text-gray-700 border border-black rounded" type="text" id="serial" name="serial" value="{{ $asset->serial }}">
 
             <label for="barcode">Barcode:</label>
-            <input class="bg-gray-200 text-gray-700 border border-black rounded" type="text" id="barcode" name="barcode" value={{ $asset->barcode }}>
+            <input class="bg-gray-200 text-gray-700 border border-black rounded" type="text" id="barcode" name="barcode" value="{{ $asset->barcode }}">
 
             <label for="company">Company:</label>
-            <input class="bg-gray-200 text-gray-700 border border-black rounded" type="text" id="company" name="company" value={{ $asset->company }}>
+            <input class="bg-gray-200 text-gray-700 border border-black rounded" type="text" id="company" name="company" value="{{ $asset->company }}">
 
             <label for="model">Model:</label>
-            <input class="bg-gray-200 text-gray-700 border border-black rounded" type="text" id="model" name="model" value={{ $asset->model }}>
+            <input class="bg-gray-200 text-gray-700 border border-black rounded" type="text" id="model" name="model" value="{{ $asset->model }}">
 
             <label for="category">Category:</label>
-            <input class="bg-gray-200 text-gray-700 border border-black rounded" type="text" id="category" name="category" value={{ $asset->category }}>
+            <input class="bg-gray-200 text-gray-700 border border-black rounded" type="text" id="category" name="category" value="{{ $asset->category }}">
         </div>
 
         <h2 class="text-2xl">Location</h2>
@@ -43,25 +43,25 @@
             </select>
 
             <label for="room">Room:</label>
-            <input class="bg-gray-200 text-gray-700 border border-black rounded" type="text" id="room" name="room" value={{ $asset->room }}>
+            <input class="bg-gray-200 text-gray-700 border border-black rounded" type="text" id="room" name="room" value="{{ $asset->room }}">
         </div>
 
         <h2 class="text-2xl">Purchasing</h2>
         <div class="grid grid-cols-4 gap-4 mb-10">
             <label for="program">Program:</label>
-            <input class="bg-gray-200 text-gray-700 border border-black rounded" type="text" id="program" name="program" value={{ $asset->program }}>
+            <input class="bg-gray-200 text-gray-700 border border-black rounded" type="text" id="program" name="program" value="{{ $asset->program }}">
 
             <label for="purchase_date">Purchase Date:</label>
-            <input class="bg-gray-200 text-gray-700 border border-black rounded" type="date" id="purchase_date" name="purchase_date" value={{ $asset->purchase_date }}>
+            <input class="bg-gray-200 text-gray-700 border border-black rounded" type="date" id="purchase_date" name="purchase_date" value="{{ $asset->purchase_date }}">
 
             <label for="expected_lifespan">Expected Lifespan:</label>
-            <input class="bg-gray-200 text-gray-700 border border-black rounded" type="number" id="expected_lifespan" name="expected_lifespan" value={{ $asset->expected_lifespan }}>
+            <input class="bg-gray-200 text-gray-700 border border-black rounded" type="number" id="expected_lifespan" name="expected_lifespan" value="{{ $asset->expected_lifespan }}">
         </div>
 
         <h2 class="text-2xl">Extra</h2>
         <div class="grid grid-cols-4 gap-4 mb-10">
             <label for="notes">Additional notes:</label>
-            <input class="bg-gray-200 text-gray-700 border border-black rounded" type="text" id="notes" name="notes" value={{ $asset->notes }}>
+            <input class="bg-gray-200 text-gray-700 border border-black rounded" type="text" id="notes" name="notes" value="{{ $asset->notes }}">
         </div>
 
         <div id="inventory">
