7a634b9ba890cef9de75f8dac485f8786fe4cb33
Josh Espinoza <joshespi@gmail.com>
2025-02-10 13:05:23 -0700

    adjusted layout of create assets page

--- src/resources/views/create_asset.blade.php
+++ src/resources/views/create_asset.blade.php
@@ -1,89 +1,113 @@
 @extends('base')
 
 @section('content')
-@if (session('status'))
-    <div>
-        {{ session('status') }}
-    </div>
-@endif
-
-@if ($errors->has('form_validation'))
-<div class="error-container">
-    <strong>
-        <p class="text-red-500">{{ $errors->first('form_validation') }}</p>
-    </strong>
-</div>
-@endif
-
-<h1 class="text-3xl mb-10">Create Asset</h1>
-<form method="POST" action="{{ route('create_asset') }}">
-    @csrf
-
-    <div>
-        <h2 class="text-2xl">General Information</h2>
-        <label for="asset_name">Asset Name:</label>
-        <input class="bg-gray-200 text-gray-700 border border-black rounded" type="text" id="asset_name" name="asset_name">
-        <label for="category">Category:</label>
-        <select class="bg-gray-200 text-gray-700 border text-sm rounded-lg p-2.5 mb-10" id="category" name="category">
-            <option disabled selected value> -- Select a category -- </option>
-            @foreach ($categories as $category)
-            <option value="{{ $category->id }}">{{ $category->display_name }}</option>
-            @endforeach
-        </select>
-        <div class="grid grid-cols-4 gap-4 mb-10">
-            <label for="serial">Serial:</label>
-            <input class="bg-gray-200 text-gray-700 border border-black rounded" type="text" id="serial" name="serial">
-
-            <label for="barcode">Barcode:</label>
-            <input class="bg-gray-200 text-gray-700 border border-black rounded" type="text" id="barcode" name="barcode">
-
-            <label for="company">Company:</label>
-            <input class="bg-gray-200 text-gray-700 border border-black rounded" type="text" id="company" name="company">
-
-            <label for="model">Model:</label>
-            <input class="bg-gray-200 text-gray-700 border border-black rounded" type="text" id="model" name="model">
+    @if (session('status'))
+        <div>
+            {{ session('status') }}
         </div>
+    @endif
 
-        <h2 class="text-2xl">Location</h2>
-        <div class="grid grid-cols-2 gap-4 mb-10">
-            <label for="site_number">Site:</label>
-            <select class="bg-gray-200 text-gray-700 border text-sm rounded-lg p-2.5" id="site_number" name="site_number">
-                <option disabled selected value> -- Select a location -- </option>
-                @foreach ($sites as $site)
-                <option value="{{ $site->site_number }}">{{ $site->display_name }}</option>
-                @endforeach
-            </select>
-
-            <label for="room">Room:</label>
-            <input class="bg-gray-200 text-gray-700 border border-black rounded" type="text" id="room" name="room">
+    @if ($errors->has('form_validation'))
+        <div class="error-container">
+            <strong>
+                <p class="text-red-500">{{ $errors->first('form_validation') }}</p>
+            </strong>
         </div>
+    @endif
 
-        <h2 class="text-2xl">Purchasing</h2>
-        <div class="grid grid-cols-2 gap-4 mb-10">
-            <label for="purchase_price">Asset Price:</label>
-            <input class="bg-gray-200 text-gray-700 border border-black rounded" type="text" id="purchase_price" name="purchase_price">
+    <h1 class="text-3xl mb-10">Create Asset</h1>
+    <form method="POST" action="{{ route('create_asset') }}">
+        @csrf
 
-            <label for="purchase_date">Purchase Date:</label>
-            <input class="bg-gray-200 text-gray-700 border border-black rounded" type="date" id="purchase_date" name="purchase_date">
+        <div>
+            <h2 class="text-2xl">General Information</h2>
+            <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 mb-10">
+                <div class="grid grid-cols-4 gap-4">
+                    <label for="asset_name">Asset Name:</label>
+                    <input class="bg-gray-200 text-gray-700 border border-black rounded col-span-3" type="text"
+                        id="asset_name" name="asset_name">
+                    <label for="category">Category:</label>
+                    <select class="bg-gray-200 text-gray-700 border text-sm rounded-lg p-2.5 mb-10 col-span-3"
+                        id="category" name="category">
+                        <option disabled selected value> -- Select a category -- </option>
+                        @foreach ($categories as $category)
+                            <option value="{{ $category->id }}">{{ $category->display_name }}</option>
+                        @endforeach
+                    </select>
+                    <label for="barcode">Barcode:</label>
+                    <input class="bg-gray-200 text-gray-700 border border-black rounded col-span-3" type="text"
+                        id="barcode" name="barcode">
+                </div>
+                <div class="grid grid-cols-4 gap-4">
+                    <label for="serial">Serial:</label>
+                    <input class="bg-gray-200 text-gray-700 border border-black rounded col-span-3" type="text"
+                        id="serial" name="serial">
 
-            <label for="projected_eol_date">Projected Asset Lifetime:</label>
-            <div>
-                <input class="bg-gray-200 text-gray-700 border border-black rounded" type="number" id="projected_lifetime" name="projected_lifetime">
-                <select class="bg-gray-200 text-gray-700 border text-sm rounded-lg p-2.5" id="projected_lifetime_units" name="projected_lifetime_units">
-                    <option value="years">Years</option>
-                    <option value="months">Months</option>
-                    <option value="days">Days</option>
-                </select>
+                    <label for="company">Company:</label>
+                    <input class="bg-gray-200 text-gray-700 border border-black rounded col-span-3" type="text"
+                        id="company" name="company">
+
+                    <label for="model">Model:</label>
+                    <input class="bg-gray-200 text-gray-700 border border-black rounded col-span-3" type="text"
+                        id="model" name="model">
+                </div>
+
+
+
             </div>
-        </div>
 
-        <h2 class="text-2xl">Extra</h2>
-        <div class="grid grid-cols-2 gap-4 mb-10">
-            <label for="notes">Description:</label>
-            <input class="bg-gray-200 text-gray-700 border border-black rounded" type="text" id="description" name="description">
-        </div>
+            <h2 class="text-2xl">Location</h2>
+            <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 mb-10">
+                <div class="grid grid-cols-4 gap-4">
+                    <label for="site_number">Site:</label>
+                    <select class="bg-gray-200 text-gray-700 border text-sm rounded-lg p-2.5 col-span-3" id="site_number"
+                        name="site_number">
+                        <option disabled selected value> -- Select a location -- </option>
+                        @foreach ($sites as $site)
+                            <option value="{{ $site->site_number }}">{{ $site->display_name }}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="grid grid-cols-4 gap-4">
+                    <label for="room">Room:</label>
+                    <input class="bg-gray-200 text-gray-700 border border-black rounded col-span-3" type="text"
+                        id="room" name="room">
+                </div>
+            </div>
 
-        <input type="submit" class="button" value="Create Asset">
-    </div>
-</form>
+            <h2 class="text-2xl">Purchasing</h2>
+            <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 mb-10">
+                <div class="grid grid-cols-4 gap-4">
+                    <label for="purchase_price">Asset Price:</label>
+                    <input class="bg-gray-200 text-gray-700 border border-black rounded col-span-3" type="text"
+                        id="purchase_price" name="purchase_price">
+                    <label for="purchase_date">Purchase Date:</label>
+                    <input class="bg-gray-200 text-gray-700 border border-black rounded col-span-3" type="date"
+                        id="purchase_date" name="purchase_date">
+                </div>
+                <div class="grid grid-cols-2">
+                    <label for="projected_eol_date">Projected Asset Lifetime:</label>
+                    <div class="grid grid-cols-2 gap-1">
+                        <input class="bg-gray-200 text-gray-700 border border-black rounded col-span-1" type="number"
+                            id="projected_lifetime" name="projected_lifetime">
+                        <select class="bg-gray-200 text-gray-700 border text-sm rounded-lg p-2.5 col-span-1"
+                            id="projected_lifetime_units" name="projected_lifetime_units">
+                            <option value="years">Years</option>
+                            <option value="months">Months</option>
+                            <option value="days">Days</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+
+            <h2 class="text-2xl">Extra</h2>
+            <div class="grid gap-4 mb-10">
+                <label for="notes">Description:</label>
+                <textarea class="bg-gray-200 text-gray-700 border border-black rounded min-h-[200px] p-3" type="text"
+                    id="description" name="description"></textarea>
+            </div>
+
+            <input type="submit" class="button" value="Create Asset">
+        </div>
+    </form>
 @endsection
