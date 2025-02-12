e8d9cae4798305904ae63ea6fdd357b40326c40d
Josh Espinoza <joshespi@gmail.com>
2025-02-12 06:45:34 -0700

    Add responsive grid layout for locations list

--- src/resources/views/all_locations.blade.php
+++ src/resources/views/all_locations.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <h1 class="text-3xl my-4">All Locations</h1>
-<ul class="grid grid-cols-3 gap-4">
+<ul class="grid grid-cols-1 sm:grid-cols-3 gap-4">
     @foreach ($location_list as $loc)
     <li><a href="{{ route('location_assets', ['location' => $loc['site_number']]) }} ">{{ $loc['display_name'] }}</a></li>
     @endforeach
