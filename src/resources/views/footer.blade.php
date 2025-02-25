b0f3ff39603bb300bcab8fb7afc50ccd3a51e1dc
Braxton Anderson <braxtona@provo.edu>
2025-02-25 09:05:26 -0700

    cleanup footer

--- src/resources/views/footer.blade.php
+++ src/resources/views/footer.blade.php
@@ -1,7 +1,3 @@
 <footer class="p-2">
-    @if ($year == '2024')
-        <div>Copyright &copy {{ $year }} Provo City School District</div>
-    @else
-        <div>Copyright &copy 2024 - {{ $year }} Provo City School District</div>
-    @endif
+    <div>Copyright &copy 2024 - {{ $year }} Provo City School District</div>
 </footer>
