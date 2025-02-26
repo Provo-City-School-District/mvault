44e1b243903e49d1cec32eab6f592a10c01104b0
Josh Espinoza <joshespi@gmail.com>
2025-02-26 08:25:03 -0700

    add migration for scheduled maintenance table

---
+++ src/database/migrations/2025_02_25_172200_create_scheduled_mainteance_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('scheduled_maintenance', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('asset_id');
+            $table->unsignedBigInteger('user_id');
+            $table->text('description');
+            $table->date('date');
+            $table->integer('interval');
+            $table->timestamps();
+            $table->foreign('asset_id')->references('id')->on('assets')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('scheduled_maintenance');
+    }
+};
