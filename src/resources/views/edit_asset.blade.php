00a071ee61d110c25828e2674ccb8977d3ea8659
Josh Espinoza <joshespi@gmail.com>
2025-03-24 14:17:44 -0600

    feed in helpdesk url from .env

--- src/resources/views/edit_asset.blade.php
+++ src/resources/views/edit_asset.blade.php
@@ -29,16 +29,17 @@
                 <div class="grid grid-cols-4 gap-4">
                     <label for="asset_name">Asset Name:</label>
                     <input class="bg-gray-200 text-gray-700 border border-black rounded col-span-3 p-1" type="text"
-                        id="asset_name" name="asset_name" value="{{ $asset->name }}" @if(!$permissions->admin && !$permissions->can_edit_assets) readonly @endif>
+                        id="asset_name" name="asset_name" value="{{ $asset->name }}"
+                        @if (!$permissions->admin && !$permissions->can_edit_assets) readonly @endif>
 
                     <label for="barcode">Barcode:</label>
                     <input class="bg-gray-200 text-gray-700 border border-black rounded col-span-3 p-1" type="text"
-                        id="barcode" name="barcode" value="{{ $asset->barcode }}" @if(!$permissions->admin && !$permissions->can_edit_assets) readonly @endif>
+                        id="barcode" name="barcode" value="{{ $asset->barcode }}"
+                        @if (!$permissions->admin && !$permissions->can_edit_assets) readonly @endif>
 
                     <label for="category">Category:</label>
                     <select class="bg-gray-200 text-gray-700 border text-sm rounded-lg p-2.5 mb-10 col-span-3"
-                        id="category" name="category"
-                        @if(!$permissions->admin && !$permissions->can_edit_assets) disabled @endif>
+                        id="category" name="category" @if (!$permissions->admin && !$permissions->can_edit_assets) disabled @endif>
                         <option disabled value> -- Select a category -- </option>
                         @foreach ($categories as $category)
                             @if ($category->id == $asset->category)
@@ -54,19 +55,20 @@
                     <label for="serial">Serial:</label>
                     <input class="bg-gray-200 text-gray-700 border border-black rounded col-span-3 p-1" type="text"
                         id="serial" name="serial" value="{{ $asset->serial }}"
-                        @if(!$permissions->admin && !$permissions->can_edit_assets) readonly @endif>
+                        @if (!$permissions->admin && !$permissions->can_edit_assets) readonly @endif>
 
-                    @if(!$permissions->admin && !$permissions->can_edit_assets)
+                    @if (!$permissions->admin && !$permissions->can_edit_assets)
                         <label for="company">Company:</label>
                         <input class="bg-gray-200 text-gray-700 border border-black rounded col-span-3 p-1" type="text"
-                            id="company" name="company" value="{{ App\Models\AssetCompany::where('id', $asset->company)->first()->name }}" readonly>
+                            id="company" name="company"
+                            value="{{ App\Models\AssetCompany::where('id', $asset->company)->first()->name }}" readonly>
                     @else
                         @livewire('asset-company-autocomplete', ['defaultValue' => $asset->company])
                     @endif
                     <label for="model">Model:</label>
                     <input class="bg-gray-200 text-gray-700 border border-black rounded col-span-3 p-1" type="text"
                         id="model" name="model" value="{{ $asset->model }}"
-                        @if(!$permissions->admin && !$permissions->can_edit_assets) readonly @endif>
+                        @if (!$permissions->admin && !$permissions->can_edit_assets) readonly @endif>
                 </div>
             </div>
 
@@ -80,8 +82,7 @@
                 <div class="grid grid-cols-4 gap-4 mb-10">
                     <label for="site_number">Site:</label>
                     <select class="bg-gray-200 text-gray-700 border text-sm rounded-lg p-2.5 col-span-3" id="site_number"
-                        name="site_number"
-                        @if(!$permissions->admin && !$permissions->can_edit_assets) disabled @endif>
+                        name="site_number" @if (!$permissions->admin && !$permissions->can_edit_assets) disabled @endif>
                         @foreach ($sites as $site)
                             @if ($site->id == $asset->site)
                                 <option value="{{ $site->site_number }}" selected>{{ $site->display_name }}</option>
@@ -96,7 +97,7 @@
                     <label for="room">Room:</label>
                     <input class="bg-gray-200 text-gray-700 border border-black rounded col-span-3" type="text"
                         id="room" name="room" value="{{ $asset->room }}"
-                        @if(!$permissions->admin && !$permissions->can_edit_assets) readonly @endif>
+                        @if (!$permissions->admin && !$permissions->can_edit_assets) readonly @endif>
                 </div>
             </div>
 
@@ -108,25 +109,24 @@
                     <label for="purchase_price">Purchase Price:</label>
                     <input class="bg-gray-200 text-gray-700 border border-black rounded col-span-3" type="number"
                         id="purchase_price" name="purchase_price" value="{{ $asset->purchase_price }}"
-                        @if(!$permissions->admin && !$permissions->can_edit_assets) readonly @endif>
+                        @if (!$permissions->admin && !$permissions->can_edit_assets) readonly @endif>
 
                     <label for="purchase_date">Purchase Date:</label>
                     <input class="bg-gray-200 text-gray-700 border border-black rounded col-span-3" type="date"
                         id="purchase_date" name="purchase_date" value="{{ $asset->purchase_date }}"
-                        @if(!$permissions->admin && !$permissions->can_edit_assets) readonly @endif>
+                        @if (!$permissions->admin && !$permissions->can_edit_assets) readonly @endif>
 
                     <label for="projected_eol_date">Projected EOL Date:</label>
                     <input class="bg-gray-200 text-gray-700 border border-black rounded col-span-3" type="date"
                         id="projected_eol_date" name="projected_eol_date" value="{{ $asset->projected_eol_date }}"
-                        @if(!$permissions->admin && !$permissions->can_edit_assets) readonly @endif>
+                        @if (!$permissions->admin && !$permissions->can_edit_assets) readonly @endif>
                 </div>
 
                 <div class="grid grid-cols-4 gap-4 mb-10">
                     <h2>Extra</h2>
                     <label for="notes" class="col-span-full">Description:</label>
                     <textarea class="bg-gray-200 text-gray-700 border border-black rounded min-h-[100px] p-3 col-span-full" type="text"
-                        id="description" name="description"
-                        @if(!$permissions->admin && !$permissions->can_edit_assets) readonly @endif>{{ $asset->description }}</textarea>
+                        id="description" name="description" @if (!$permissions->admin && !$permissions->can_edit_assets) readonly @endif>{{ $asset->description }}</textarea>
                 </div>
             </div>
 
@@ -144,18 +144,15 @@
                 @csrf
                 <label for="work_description" class="col-span-full">Description of Work:</label>
                 <textarea class="bg-gray-200 text-gray-700 border border-black rounded min-h-[100px] p-3 col-span-full"
-                    id="work_description" name="description"
-                    @if(!$permissions->admin && !$permissions->can_do_work) readonly @endif></textarea>
+                    id="work_description" name="description" @if (!$permissions->admin && !$permissions->can_do_work) readonly @endif></textarea>
 
                 <label for="work_date" class="col-span-2">Date of work:</label>
                 <input class="bg-gray-200 text-gray-700 border border-black rounded col-span-4" type="date"
-                    id="work_date" name="date"
-                    @if(!$permissions->admin && !$permissions->can_do_work) readonly @endif>
+                    id="work_date" name="date" @if (!$permissions->admin && !$permissions->can_do_work) readonly @endif>
 
                 <label for="ticket_id" class="col-span-2">Ticket Number:</label>
                 <input class="bg-gray-200 text-gray-700 border border-black rounded col-span-4" type="number"
-                    id="ticket_id" name="ticket_id"
-                    @if(!$permissions->admin && !$permissions->can_do_work) readonly @endif>
+                    id="ticket_id" name="ticket_id" @if (!$permissions->admin && !$permissions->can_do_work) readonly @endif>
 
                 <input type="submit" class="button col-span-full max-w-fit" value="Add Work Done">
             </form>
@@ -180,7 +177,7 @@
                             <td class="border px-4 py-2">{{ $work->description }}</td>
                             <td class="border px-4 py-2">{{ $work->user->name }}</td>
                             <td class="border px-4 py-2"><a
-                                    href="https://help-stage.provo.edu/controllers/tickets/edit_ticket.php?id={{ $work->ticket_id }}">{{ $work->ticket_id }}</a>
+                                    href="{{ env('HELP_URL') }}/controllers/tickets/edit_ticket.php?id={{ $work->ticket_id }}">{{ $work->ticket_id }}</a>
                             </td>
                             <td class="border px-4 py-2">{{ $work->ticket_status }}</td>
                         </tr>
@@ -196,18 +193,15 @@
                 @csrf
                 <label for="maintenance_description" class="col-span-full">Description of Maintenance:</label>
                 <textarea class="bg-gray-200 text-gray-700 border border-black rounded min-h-[100px] p-3 col-span-full"
-                    id="maintenance_description" name="description"
-                    @if(!$permissions->admin && !$permissions->can_schedule_work) readonly @endif></textarea>
+                    id="maintenance_description" name="description" @if (!$permissions->admin && !$permissions->can_schedule_work) readonly @endif></textarea>
 
                 <label for="maintenance_date" class="col-span-2">Date of Maintenance:</label>
                 <input class="bg-gray-200 text-gray-700 border border-black rounded col-span-4" type="date"
-                    id="maintenance_date" name="date"
-                    @if(!$permissions->admin && !$permissions->can_schedule_work) readonly @endif>
+                    id="maintenance_date" name="date" @if (!$permissions->admin && !$permissions->can_schedule_work) readonly @endif>
 
                 <label for="maintenance_interval" class="col-span-2">Interval (days):</label>
                 <input class="bg-gray-200 text-gray-700 border border-black rounded col-span-4" type="number"
-                    id="maintenance_interval" name="interval"
-                    @if(!$permissions->admin && !$permissions->can_schedule_work) readonly @endif>
+                    id="maintenance_interval" name="interval" @if (!$permissions->admin && !$permissions->can_schedule_work) readonly @endif>
                 <input type="submit" class="button col-span-full max-w-fit" value="Schedule Maintenance">
             </form>
         </div>
