881048c987c6176aaa308040f8119ba729037776
Josh Espinoza <joshespi@gmail.com>
2025-03-24 09:58:02 -0600

    added dataTables library

--- src/resources/views/base.blade.php
+++ src/resources/views/base.blade.php
@@ -5,7 +5,9 @@
     <title>Maintenance Vault</title>
     <link rel="stylesheet" href="{{ asset('assets/css/variables-common.css') }}?v={{ env('ASSET_VERSION') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/tailwind.css') }}?v={{ env('ASSET_VERSION') }}">
+    <link rel="stylesheet" href="{{ asset('assets/DataTables/datatables.min.css') }}?v={{ env('ASSET_VERSION') }}">
 </head>
+
 <body>
     @include('header')
 
@@ -14,9 +16,12 @@
     </main>
 
     @include('footer')
-    <script src="{{ asset('assets/js/main.js') }}"></script>
+    <script src="{{ asset('assets/js/main.js') }}?v={{ env('ASSET_VERSION') }}"></script>
+    <script src="{{ asset('assets/DataTables/datatables.min.js') }}?v={{ env('ASSET_VERSION') }}"></script>
     @yield('script')
     @livewireScripts
     @livewireStyles
 </body>
+@yield('config')
+
 </html>
