497bf47dcff1596adbcf6ac3c896f33f643451db
Josh Espinoza <joshespi@gmail.com>
2025-03-24 14:23:14 -0600

    added primary key

--- src/database/migrations/2025_03_24_140403_create_permissions_table.php
+++ src/database/migrations/2025_03_24_140403_create_permissions_table.php
@@ -12,6 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('permissions', function (Blueprint $table) {
+            $table->id();
             $table->bigInteger('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users');
             $table->boolean('admin')->default(0);
