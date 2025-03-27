eafbc8c27971950ec4552029e84ba68ff304f4d9
Josh Espinoza <joshespi@gmail.com>
2025-03-27 09:12:39 -0600

    restrict menu visibility based on user permissions for asset creation and admin access

--- src/resources/views/header.blade.php
+++ src/resources/views/header.blade.php
@@ -3,17 +3,25 @@
     <link rel="stylesheet" href="{{ asset('assets/css/header.css') }}?v={{ env('ASSET_VERSION') }}">
     @auth
         <nav class="topnav" id="myTopnav">
-            <a href="{{ route('create_asset') }}" class="{{ request()->is('create_asset') ? 'active' : '' }}">Create Asset</a>
-            <a href="{{ route('all_locations') }}" class="{{ request()->is('all_locations') ? 'active' : '' }}">Locations</a>
+            {{-- Admins can see all menu items --}}
+            @if (Auth::user()->permissions->admin || Auth::user()->permissions->can_create_assets)
+                <a href="{{ route('create_asset') }}" class="{{ request()->is('create_asset') ? 'active' : '' }}">Create
+                    Asset</a>
+            @endif
+            <a href="{{ route('all_locations') }}"
+                class="{{ request()->is('all_locations') ? 'active' : '' }}">Locations</a>
             <div class="dropdown">
-                <a href="{{ route('search') }}" class="dropbtn {{ request()->is('search') || request()->is('search_eol') ? 'active' : '' }}">Search</a>
+                <a href="{{ route('search') }}"
+                    class="dropbtn {{ request()->is('search') || request()->is('search_eol') ? 'active' : '' }}">Search</a>
                 <div class="dropdown-content" style="left:0;">
                     <a href="{{ route('search') }}">Search</a>
                     <a href="{{ route('search_eol') }}">Search EOL</a>
                 </div>
             </div>
             <a href="{{ route('profile') }}" class="{{ request()->is('profile') ? 'active' : '' }}">Profile</a>
-            <a href="{{ route('admin') }}" class="{{ request()->is('active') ? 'active' : '' }}">Admin</a>
+            @if (Auth::user()->permissions->admin)
+                <a href="{{ route('admin') }}" class="{{ request()->is('admin') ? 'active' : '' }}">Admin</a>
+            @endif
             <a href="{{ route('logout') }}">Logout</a>
             <a href="javascript:void(0);" class="icon" onclick="toggleNav()">
                 &#9776;
