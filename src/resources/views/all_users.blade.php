c7b18f461f8ab22f5afff7245c95768ed2a2c0b9
Josh Espinoza <joshespi@gmail.com>
2025-03-27 09:18:57 -0600

    display users permission rather than authenticated users permission

--- src/resources/views/all_users.blade.php
+++ src/resources/views/all_users.blade.php
@@ -1,29 +1,28 @@
 @extends('base')
 
 @section('content')
-<h1>All Users</h1>
-<table id="usersTable">
-    <thead>
-        <tr>
-            <th>Email</th>
-            <th>Name</th>
-            <th>Is Admin</th>
-            <th>Last Login</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach ($users as $user)
-        <tr>
-            <td><a href="{{ route("manage_user", ['id' => $user->id]) }}">{{ $user->email }}</a></td>
-            <td>{{ $user->name }}</td>
-            <td>{{ Illuminate\Support\Facades\Auth::user()->permissions->admin ? "Yes" : "No" }}</td>
-            <td>{{ $user->last_login }}</td>
-        </tr>
-        @endforeach
-    </tbody>
-</table>
-@livewire('allowed-users')
-
+    <h1>All Users</h1>
+    <table id="usersTable">
+        <thead>
+            <tr>
+                <th>Email</th>
+                <th>Name</th>
+                <th>Is Admin</th>
+                <th>Last Login</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($users as $user)
+                <tr>
+                    <td><a href="{{ route('manage_user', ['id' => $user->id]) }}">{{ $user->email }}</a></td>
+                    <td>{{ $user->name }}</td>
+                    <td>{{ $user->permissions->admin ? 'Yes' : 'No' }}</td>
+                    <td>{{ $user->last_login }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+    @livewire('allowed-users')
 @endsection
 @section('config')
     <script>
